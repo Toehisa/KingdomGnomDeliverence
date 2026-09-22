8775614a652105da06fea64c89c346a59c2ae681
Toehisa <tigrera9999eagle@gmail.com>
2026-09-22 22:37:05 +0500

    [arch1] Абстрактный класс MenuSelector

---
+++ src/main/java/com/kingdom/gnome/presentation/MenuSelector.java
@@ -0,0 +1,22 @@
+package com.kingdom.gnome.presentation;
+
+import com.sun.tools.javac.Main;
+
+import java.util.ArrayList;
+
+abstract public class MenuSelector {
+    List<Menu> menus;
+    int curMenuIndex = -1;
+
+    MenuSelector() {
+        menus = new ArrayList<>();
+    }
+    MenuSelector(int menuCount) {
+        menus = new ArrayList<>(menuCount);
+    }
+
+    abstract Main currentMenu();
+    abstract void showMenu();
+    abstract void selectMenu();
+    abstract void switchMenu();
+}

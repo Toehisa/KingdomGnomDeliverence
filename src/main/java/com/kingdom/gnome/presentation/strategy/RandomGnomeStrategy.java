4f017b86a2896cee9ddfa4504e82d5bb2f77e45a
JenfX <Screamer42@yandex.ru>
2026-09-23 20:12:46 +0700

    Добавлено 3 сущности для стратегии

--- src/main/java/com/kingdom/gnome/presentation/strategy/RandomGnomeStrategy.java
+++ src/main/java/com/kingdom/gnome/presentation/strategy/RandomGnomeStrategy.java
@@ -1,11 +1,19 @@
 package com.kingdom.gnome.presentation.strategy;
 
 import com.kingdom.gnome.dao.entity.Gnome;
+import com.kingdom.gnome.dao.entity.Random.EmailDomains;
+import com.kingdom.gnome.dao.entity.Random.EmailNames;
+import com.kingdom.gnome.dao.entity.Random.GnomeNames;
 
 import java.util.List;
 import java.util.Scanner;
 
 public class RandomGnomeStrategy implements GnomeCreationStrategy {
+
+    private final String[] nameArray = GnomeNames.getNameArray();
+    private final String[] emailArray = EmailNames.getEmailArray();
+    private final String[] domainArray = EmailDomains.getDomainArray();
+
     @Override
     public List<Gnome> create(Scanner scanner) {
         return null;

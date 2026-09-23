d6c3927f691c80333bde5b94fbf49ca3f02f6897
JenfX <Screamer42@yandex.ru>
2026-09-23 20:09:41 +0700

    Создан класс содержащий имена гномов для рандомной генерации

---
+++ src/main/java/com/kingdom/gnome/dao/entity/Random/GnomeNames.java
@@ -0,0 +1,9 @@
+package com.kingdom.gnome.dao.entity.Random;
+
+public class GnomeNames {
+    private static final String[] nameArray = {"", ""};
+
+    public static String[] getNameArray() {
+        return nameArray;
+    }
+}

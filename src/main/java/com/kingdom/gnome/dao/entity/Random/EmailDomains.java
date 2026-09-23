c50b046f495ef6f5decf0c4385c953d3f8f752d8
JenfX <Screamer42@yandex.ru>
2026-09-23 23:53:49 +0700

    Заполнение списка данных для рандома

--- src/main/java/com/kingdom/gnome/dao/entity/Random/EmailDomains.java
+++ src/main/java/com/kingdom/gnome/dao/entity/Random/EmailDomains.java
@@ -1,7 +1,8 @@
 package com.kingdom.gnome.dao.entity.Random;
 
 public class EmailDomains {
-    private static final String[] domainArray = {"@mail.ru", "@gmail.com", "yandex.ru", "icloud.com", "outlook.com"};
+    private static final String[] domainArray = {"@mail.ru", "@gmail.com", "@yandex.ru", "@icloud.com",
+            "@outlook.com", "@hotmail.com", "@bk.ru"};
 
     public static String[] getDomainArray() {
         return domainArray;
--- src/main/java/com/kingdom/gnome/dao/entity/Random/EmailNames.java
+++ src/main/java/com/kingdom/gnome/dao/entity/Random/EmailNames.java
@@ -1,7 +1,13 @@
 package com.kingdom.gnome.dao.entity.Random;
 
 public class EmailNames {
-    private static final String[] emailArray = {"", ""};
+    private static final String[] emailArray = {"yoho-seyite73", "cudefiy_ewa7", "bux_abaseco32", "nofof_ucava41",
+    "zipoji_wase93", "fer-agogoto84", "kejumok-oge50", "baxoc_anuba16", "forasu_feho13", "dafe-meniso59",
+    "cipi_yiyofe67", "fez_ixoxoco59", "regure_weda62", "nekurot-iwe75", "kip_ugivuyi3", "fibuki_sami55",
+    "yen_ukadowu54", "ziwada-cici4", "baye-vayigo56", "baw_ipuhudi99", "gogi_yudowe9", "nelom_uvobu86",
+    "joker", "thor1", "bear9", "dragon92x921neo", "frodon2010tiger", "odin2012nova99t", "bear1992eagleVk",
+    "zelda1986odin99", "lion29supportKx", "moon2008vaderrx", "beta1990bear12m", "tiger1992eagle2",
+            "wolf703ice8lion", "fire84ra865thor", "link69neo6zelda", "zeus2hq97foxvvz"};
 
     public static String[] getEmailArray() {
         return emailArray;

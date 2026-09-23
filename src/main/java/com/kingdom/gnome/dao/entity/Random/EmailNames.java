c1114ea8edda3fc19413d8639ac759917c67d02b
JenfX <Screamer42@yandex.ru>
2026-09-23 20:10:29 +0700

    Создан класс содержащий "emailnames" гномов для рандомной генерации

---
+++ src/main/java/com/kingdom/gnome/dao/entity/Random/EmailNames.java
@@ -0,0 +1,9 @@
+package com.kingdom.gnome.dao.entity.Random;
+
+public class EmailNames {
+    private static final String[] emailArray = {"", ""};
+
+    public static String[] getEmailArray() {
+        return emailArray;
+    }
+}

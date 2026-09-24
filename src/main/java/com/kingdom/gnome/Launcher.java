2eba1f1e1960fb1f47f20db3e3c8c4ee1a338fe9
alexvils7766 <alexvils7766@gmail.com>
2026-09-25 04:53:44 +0700

    Передали inputReader и создали его в Launcher

--- src/main/java/com/kingdom/gnome/Launcher.java
+++ src/main/java/com/kingdom/gnome/Launcher.java
@@ -5,6 +5,7 @@ import com.kingdom.gnome.presentation.menu.providers.MainMenuProvider;
 import com.kingdom.gnome.presentation.menu.providers.MenuProvider;
 import com.kingdom.gnome.presentation.menu.selectors.MainMenuSelector;
 import com.kingdom.gnome.presentation.menu.selectors.MenuSelector;
+import com.kingdom.gnome.presentation.input.ConsoleInputReader;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +18,8 @@ public class Launcher {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)){
             List<Gnome> gnomes = new ArrayList<>();
-            MenuProvider menuProvider = new MainMenuProvider(gnomes);
+            ConsoleInputReader inputReader = new ConsoleInputReader(scanner);
+            MenuProvider menuProvider = new MainMenuProvider(gnomes,inputReader);
             MenuSelector menuSelector = new MainMenuSelector(menuProvider);
             menuSelector.run(scanner);
         }

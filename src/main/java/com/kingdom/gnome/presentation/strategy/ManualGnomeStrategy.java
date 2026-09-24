c7f843bcc613eafa9a6bf1ae69d9a26e26dadd32
alexvils7766 <alexvils7766@gmail.com>
2026-09-24 19:52:23 +0700

    [task4]-Реализовал первую ступень метода ручного ввода

--- src/main/java/com/kingdom/gnome/presentation/strategy/ManualGnomeStrategy.java
+++ src/main/java/com/kingdom/gnome/presentation/strategy/ManualGnomeStrategy.java
@@ -11,6 +11,25 @@ public class ManualGnomeStrategy implements GnomeCreationStrategy {
 
     @Override
     public List<Gnome> create(Scanner scanner) {
-        return List.of();
-    }
-}
+
+        List<Gnome> gnomes = new java.util.ArrayList<>();
+
+        System.out.print("Введите количество гномов: ");
+        int count = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < count; i++) {
+
+            System.out.println("\nГном №" + (i + 1));
+
+            System.out.print("Введите имя гнома: ");
+            String name = scanner.nextLine().trim();
+
+            System.out.println("Выберите роль:");
+
+            GnomeRole[] roles = GnomeRole.values();
+
+            for (int j = 0; j < roles.length; j++) {
+                System.out.println(
+                        (j + 1) + ". " + roles[j].getTitle()
+                );
+            }

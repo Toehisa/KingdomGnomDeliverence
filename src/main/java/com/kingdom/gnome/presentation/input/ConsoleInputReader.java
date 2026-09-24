f972aa64d038c1e42fae927447b7a949d238a028
alexvils7766 <alexvils7766@gmail.com>
2026-09-25 04:25:46 +0700

    Закидываем "ошибку" в отдельный файл

--- src/main/java/com/kingdom/gnome/presentation/input/ConsoleInputReader.java
+++ src/main/java/com/kingdom/gnome/presentation/input/ConsoleInputReader.java
@@ -1,4 +1,18 @@
 package com.kingdom.gnome.presentation.input;
 
+import java.util.Scanner;
+
 public class ConsoleInputReader {
+    public int readInt(Scanner scanner) {
+        while (true) {
+            String input = scanner.nextLine().trim();
+
+            try {
+                return Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Ты мне вводи цифры, а не буквы.");
+                System.out.print("Твой выбор: ");
+            }
+        }
+    }
 }

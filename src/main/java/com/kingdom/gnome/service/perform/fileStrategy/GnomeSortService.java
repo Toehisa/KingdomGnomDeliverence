a82a54f9cf07606a7eeb7cf2d647751676ac3eb1
alexvils7766 <alexvils7766@gmail.com>
2026-09-25 01:55:26 +0700

    [Task6]-Реализовал сортировку по имени

--- src/main/java/com/kingdom/gnome/service/perform/fileStrategy/GnomeSortService.java
+++ src/main/java/com/kingdom/gnome/service/perform/fileStrategy/GnomeSortService.java
@@ -1,4 +1,14 @@
 package com.kingdom.gnome.service.perform.fileStrategy;
 
+import com.kingdom.gnome.dao.entity.Gnome;
+
+import java.util.Comparator;
+import java.util.List;
+
 public class GnomeSortService {
-}
+
+    public void sortByName(List<Gnome> gnomes) {
+        gnomes.sort(
+                Comparator.comparing(Gnome::getName)
+        );
+    }
